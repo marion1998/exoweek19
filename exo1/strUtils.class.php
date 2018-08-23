c3f38593dc058681c14c0460f495050a7f686a8e
marion1998 <marion281098@outlook.fr>
2018-08-23 11:24:39 +0200

    call class

--- exo1/strUtils.class.php
+++ exo1/strUtils.class.php
@@ -1,14 +1,28 @@
 <?php
     class StrUtils {
         private $str;
-        function __construct($string) {
+        public function __construct($string) {
             $this->str = $string;
         }
 
-        public function italic();
+        public function italic(){
+            return "<h4> Italic : </h4> <i>" . $this->str . "</i> <hr>";
+                }
+        
+        public function bold(){
+            return "<h4> Bold : </h4>  <b>" . $this->str . "</b> <hr>";
+        }
+
+        public function underline(){
+            return "<h4> Underline : </h4> <u>" . $this->str . "</u> <hr>";
+        }
+
+        public function capitalize(){
+            return "<h4> Capitalize : </h4> <>" . $this->str . "</> <hr>";
+        }
     }
 
-    $myStr = new StrUtils('hello');
+    // $myStr = new StrUtils('hello');
 
 
 
